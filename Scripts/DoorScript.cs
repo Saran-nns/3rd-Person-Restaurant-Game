7fce968976d84b0af94153a491513043907650e9
Saran N Subramaniyan <sarannsworld@gmail.com>
2017-04-26 04:58:59 +0530

    Add files via upload

---
+++ Scripts/DoorScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+	public bool open=false;
+	public float openAngle=-160f;
+	public float closeAngle=0f;
+	public float doorSpeed=2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void ChangeDoorState()
+	{
+		open = !open;
+
+	}
+
+	void Update () {
+		if (open) {
+		
+			Quaternion targetRotation = Quaternion.Euler (0, openAngle, 0);
+			transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation, doorSpeed * Time.deltaTime);
+		} 
+		else
+		{
+			Quaternion targetRotation2 = Quaternion.Euler (0, closeAngle, 0);
+			transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation2, doorSpeed * Time.deltaTime);
+		}
+	
+	}
+}
---
+++ Scripts/interactionScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class interactionScript : MonoBehaviour {
+
+	public float interactDistance=3f;
+
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Ray ray = new Ray (transform.position, transform.forward);
+			RaycastHit hit;
+			if (Physics.Raycast(ray,out hit,interactDistance))
+			{
+				if (hit.collider.CompareTag ("DoorToilet"))
+				{
+					hit.collider.transform.parent.GetComponent<DoorScript> ().ChangeDoorState ();
+			    }
+
+		    }
+
+	    }
+   }
+}
